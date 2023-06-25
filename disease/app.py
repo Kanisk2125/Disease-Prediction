3304e3751c0e210464322f29a9a658cbfbd1f1c8
Kanisk2125 <105097052+Kanisk2125@users.noreply.github.com>
2023-06-25 15:39:00 +0530

    Update app.py

--- disease/app.py
+++ disease/app.py
@@ -15,9 +15,10 @@ import pandas as pd
 #diabetes_model = pickle.load(open('C:/Users/HP/OneDrive/Desktop/disease/diabetes_model.sav', 'rb'))
 #heart_disease_model = pickle.load(open('C:/Users/HP/OneDrive/Desktop/disease/heart_disease_model.sav','rb'))
 #parkinsons_model = pickle.load(open('C:/Users/HP/OneDrive/Desktop/disease/parkinsons_model.sav','rb'))
-diabetes_model = pd.read_csv('diabetes_model.csv')
-heart_disease_model =pd.read_csv('heart_disease_model.csv')
-parkinsons_model =pd.read_csv('parkinsons_model.csv')
+diabetes_model = pd.read_csv(open('diabetes_model.csv','rb'))
+
+heart_disease_model =pd.read_csv(open('heart_disease_model.csv','rb'))
+parkinsons_model =pd.read_csv(open('parkinsons_model.csv','rb'))
 
 
 # sidebar for navigation
